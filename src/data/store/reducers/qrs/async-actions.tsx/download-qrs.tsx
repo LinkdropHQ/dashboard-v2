54dfd876d22d7e4eaf648b53eb993428754adfce
spacehaz <spacehaz@gmail.com>
2022-11-02 18:58:38 +0300

    added fix for qr worker

--- src/data/store/reducers/qrs/async-actions.tsx/download-qrs.tsx
+++ src/data/store/reducers/qrs/async-actions.tsx/download-qrs.tsx
@@ -4,16 +4,11 @@ import { QRsActions } from '../types'
 import { RootState } from 'data/store'
 import { downloadBase64FilesAsZip } from 'helpers'
 import { TQRItem } from "types"
-import QRCodeStyling from 'qr-code-styling-bigmac'
-import { decrypt } from 'lib/crypto'
-const ledgerImage = `                                                                                                                                                                                            
-  <svg width="60" height="51" viewBox="0 0 60 51" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="60" height="51" fill="black"/>
-    <path d="M5.40002 34.3V46.4H23.9V43.7H8.10003V34.2H5.40002V34.3ZM51.9 34.3V43.8H36.1V46.5H54.6V34.3H51.9ZM24 15.8V34.3H36.1V31.9H26.6V15.9H24V15.8ZM5.40002 3.60001V15.7H8.10003V6.3H23.9V3.60001H5.40002ZM36.1 3.60001V6.3H51.9V15.8H54.6V3.60001H36.1Z" fill="white"/>
-  </svg>                                                                                                                                                                                                      
-`
-
-const ledgerImageUpdated = "data:image/svg+xml," + encodeURIComponent(ledgerImage)
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Worker from 'worker-loader!web-workers/qrs-worker'
+import { QRsWorker } from 'web-workers/qrs-worker'
+import { wrap, Remote, proxy } from 'comlink';
+import { sleep } from 'helpers'
 
 const {
   REACT_APP_CLAIM_APP
@@ -42,48 +37,24 @@ const downloadQRs = ({
     try {
       if (!dashboardKey) { return alert('dashboardKey is not provided') }
       if (!qrsArray) { return alert('qrsArray is not provided') }
-      let qrs: Blob[] = []
       const start = +(new Date())
-      const initialQR = new QRCodeStyling({
-        data: `${REACT_APP_CLAIM_APP}/#/qr/`,
+
+      const updateProgressbar = async (value: number) => {
+        dispatch(actionsQR.setDownloadLoader(value))
+        await sleep(1)
+      }
+
+      const RemoteChannel = wrap<typeof QRsWorker>(new Worker())
+      const qrsWorker: Remote<QRsWorker> = await new RemoteChannel(proxy(updateProgressbar));
+  
+      const qrs = await qrsWorker.downloadQRs(
+        qrsArray,
         width,
         height,
-        margin: width / 60,
-        type: 'svg',
-        cornersSquareOptions: {
-          color: "#FFF",
-          type: 'square'
-        },
-        cornersDotOptions: {
-          color: "#FFF",
-          type: 'square'
-        },
-        dotsOptions: {
-          color: "#FFF",
-          type: "dots"
-        },
-        backgroundOptions: {
-          color: "#000",
-       },
-        image: ledgerImageUpdated,
-        imageOptions: {
-          margin: width / 60,
-          imageSize: 0.5,
-          crossOrigin: 'anonymous',
-        }
-      })
-      for (let i = 0; i < qrsArray.length; i++) {
-        const decrypted_qr_secret = decrypt(qrsArray[i].encrypted_qr_secret, dashboardKey)
-        initialQR.update({
-          data: `${REACT_APP_CLAIM_APP}/#/qr/${decrypted_qr_secret}` 
-        })
-
-        const blob = await initialQR.getRawData('png')
-        if (!blob) { continue }
-        qrs = [...qrs, blob]
-        const percentageFinished = Math.round((i + 1) / qrsArray.length * 100) / 100
-        dispatch(actionsQR.setDownloadLoader(percentageFinished))
-      }
+        dashboardKey,
+        REACT_APP_CLAIM_APP
+      )
+      console.log({ qrs })
 
       console.log((+ new Date()) - start)
 
--- src/web-workers/qrs-worker.ts
+++ src/web-workers/qrs-worker.ts
@@ -1,9 +1,19 @@
+/* gobal OffscreenCanvas */
+
 import { expose } from 'comlink';
 import { TQRItem, TLinkDecrypted } from 'types'
 import * as wccrypto from '@walletconnect/utils/dist/esm'
 import { ethers } from 'ethers'
 import { decrypt, encrypt } from 'lib/crypto'
+import QRCodeStyling from 'qr-code-styling-bigmac'
+const ledgerImage = `                                                                                                                                                                                            
+  <svg width="60" height="51" viewBox="0 0 60 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="60" height="51" fill="black"/>
+    <path d="M5.40002 34.3V46.4H23.9V43.7H8.10003V34.2H5.40002V34.3ZM51.9 34.3V43.8H36.1V46.5H54.6V34.3H51.9ZM24 15.8V34.3H36.1V31.9H26.6V15.9H24V15.8ZM5.40002 3.60001V15.7H8.10003V6.3H23.9V3.60001H5.40002ZM36.1 3.60001V6.3H51.9V15.8H54.6V3.60001H36.1Z" fill="white"/>
+  </svg>                                                                                                                                                                                                      
+`
 
+const ledgerImageUpdated = "data:image/svg+xml," + encodeURIComponent(ledgerImage)
 
 export class QRsWorker {
   private cb: (value: number) => void;
@@ -41,9 +51,49 @@ export class QRsWorker {
   }
 
   public async downloadQRs (
-
+    qrsArray: TQRItem[],
+    width: number,
+    height: number,
+    dashboardKey: string,
+    claimAppUrl?: string
   ) {
+    let qrs: Blob[] = []
 
+    for (let i = 0; i < qrsArray.length; i++) {
+      const decrypted_qr_secret = decrypt(qrsArray[i].encrypted_qr_secret, dashboardKey)
+      const qrCode = new QRCodeStyling({
+        data: `${claimAppUrl}/#/qr/${decrypted_qr_secret}`,
+        width,
+        height,
+        margin: width / 60,
+        type: 'canvas',
+        cornersSquareOptions: {
+          color: "#FFF",
+          type: 'square'
+        },
+        cornersDotOptions: {
+          color: "#FFF",
+          type: 'square'
+        },
+        dotsOptions: {
+          color: "#FFF",
+          type: "dots"
+        },
+        backgroundOptions: {
+          color: "#000",
+       }
+      })
+
+      const blob = await qrCode.getRawData('png')
+      if (!blob) { continue }
+      qrs.push(blob)
+      const percentageFinished = Math.round(i / qrsArray.length * 100) / 100
+      if (this.currentPercentageFinished < percentageFinished) {
+        this.currentPercentageFinished = percentageFinished
+        this.cb(this.currentPercentageFinished)
+      }
+    }
+    return qrs
   }
 
   public mapQrsWithLinks (
