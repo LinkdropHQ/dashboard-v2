9c3085d5a797be7732a88a75d561ae2d548d1be4
spacehaz <spacehaz@gmail.com>
2022-11-12 22:42:10 +0100

    added fix for await of zip download

--- src/data/store/reducers/qrs/async-actions.tsx/download-qrs.tsx
+++ src/data/store/reducers/qrs/async-actions.tsx/download-qrs.tsx
@@ -89,7 +89,7 @@ const downloadQRs = ({
 
       console.log((+ new Date()) - start)
 
-      downloadBase64FilesAsZip('png', result.flat(), qrSetName)
+      await downloadBase64FilesAsZip('png', result.flat(), qrSetName)
       currentPercentage = 0
       terminateWorkers(workers)
       dispatch(actionsQR.setDownloadLoader(0))
