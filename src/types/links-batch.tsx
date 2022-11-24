31ce15b942a6b4104129e073681a832596076766
spacehaz <spacehaz@gmail.com>
2022-11-24 14:32:00 +0100

    added fix for upload batch via endpoint

--- src/types/links-batch.tsx
+++ src/types/links-batch.tsx
@@ -4,7 +4,9 @@ type TLinksBatch = {
   claim_links: TLink[],
   sponsored?: boolean,
   batch_description: string,
-  created_at?: string
+  created_at?: string,
+  claim_links_count: number,
+  batch_id: string
 }
 
 export default TLinksBatch
