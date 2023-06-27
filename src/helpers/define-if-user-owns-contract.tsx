154ea9172a24fe1bcede15d7c3789ebd7c9026e2
spacehaz <spacehaz@gmail.com>
2023-06-27 18:05:13 +0200

    added updates for contract check

--- src/helpers/define-if-user-owns-contract.tsx
+++ src/helpers/define-if-user-owns-contract.tsx
@@ -1,10 +1,25 @@
 import { Alchemy } from 'alchemy-sdk'
 import { defineAlchemyNetwork } from 'helpers'
+import { TTokenType } from 'types'
+import { ethers } from 'ethers'
+import { ERC721Contract, ERC1155Contract } from 'abi'
 const { REACT_APP_ALCHEMY_API_KEY } = process.env
 
-type TDefineIfUserOwnsContract = (userAddress: string, tokenAddress: string, chainId: number) => Promise<boolean>
+type TDefineIfUserOwnsContract = (
+  userAddress: string,
+  tokenAddress: string,
+  chainId: number,
+  tokenType: TTokenType,
+  signer: any
+) => Promise<boolean>
 
-const defineIfUserOwnsContract: TDefineIfUserOwnsContract = async (userAddress, tokenAddress, chainId) => {
+const defineIfUserOwnsContract: TDefineIfUserOwnsContract = async (
+  userAddress,
+  tokenAddress,
+  chainId,
+  tokenType,
+  signer
+) => {
   try {
     const alchemy = new Alchemy({
       apiKey: REACT_APP_ALCHEMY_API_KEY,
@@ -15,6 +30,15 @@ const defineIfUserOwnsContract: TDefineIfUserOwnsContract = async (userAddress, 
     console.log({
       err
     })
+    if (tokenType === 'ERC721') {
+      const contractInstance = new ethers.Contract(tokenAddress, ERC721Contract.abi, signer)
+      const balance = await contractInstance.balanceOf(userAddress)
+      if (String(balance) === '0') {
+        return false
+      }
+    } else if (tokenType === 'ERC1155') {
+      
+    }
     return false
   }
 }
